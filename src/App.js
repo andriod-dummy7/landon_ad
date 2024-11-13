518aa90a767544e5abbd8801c91a61d59b8443b9
gurmev <gurmev@amazon.com>
2024-11-13 00:54:28 -0600

    First challenge commit on repor

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React in less than a day
+          Completing my first challenge of react serverless web application
         </a>
       </header>
     </div>
